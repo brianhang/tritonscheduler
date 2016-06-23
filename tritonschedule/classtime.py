eda6d3ca41ebe476cf821ef7ebf84071616f13cd
Brian Hang <hangwithbrian@gmail.com>
2016-06-23 16:31:12 -0700

    Implemented ClassTime class

--- tritonschedule/classtime.py
+++ tritonschedule/classtime.py
@@ -1,26 +1,80 @@
 #!/usr/bin/env python
 
-SECONDS_MIN = 60
-SECONDS_HOUR = SECONDS_MIN * 60
+import re
 
-class ClassTime:
+HOUR = 60
+
+REGEX_CLASSTIME = "([a-zA-Z]+)[\s+](\d*\d:\d\d[ap])-(\d*\d:\d\d[ap])" 
+REGEX_TIME = "(\d*\d):(\d\d)([ap])"
+REGEX_DAY = "([A-Z][a-z]*)"
+
+MAX_HOURS = 12
+MAX_MINS = 60
+
+INDEX_DAYS = 1
+INDEX_START = 2
+INDEX_FINISH = 3
+INDEX_HOURS = 1
+INDEX_MINS = 2
+INDEX_PERIOD = 3
+
+class ClassTime(object):
     days = "MTuWThF"
     start = "00:00a"
     finish = "11:59p"
     startTime = 0
-    finishTime = (11 * SECONDS_HOUR) + (59 * SECONDS_MIN)
+    finishTime = (11 * HOUR) + 59
+
+    @staticmethod
+    def toMinutes(hour, minutes, period):
+        """
+        Converts hours and minutes into the number of minutes within the day. 
+
+        :param hour: the hour number
+        :param minutes: the minute number
+        :returns: the number of minutes representing the hour:minutes time if
+        successfully converted, -1 otherwise
+        """
+        period = period.lower()
+
+        # Check that the period is valid.
+        if period != "a" and period != "p":
+            return -1
+
+        # Return appropriate values for noon and midnight.
+        if hour == MAX_HOURS:
+            return HOUR * (MAX_HOURS if period == "p" else 0) + minutes
+
+        # If PM, then add 12 hours to convert to a 24 hour time.
+        if period == "p":
+            hour += MAX_HOURS
+
+        # Convert hours to minutes.
+        hour *= HOUR
+
+        return hour + minutes
 
     @classmethod
-    def fromString(myClass, date):
+    def fromString(thisClass, date):
         """
         Creates a ClassTime object given a string that is in the format of
-        weekday followed by a start time, a dash, and a finish time.
+        weekday followed by a start time, a dash, and a finish time. Note that
+        None will be returned if the given date string is not in a valid format.
 
-        :param myClass: the ClassTime class
+        :param thisClass: the ClassTime class
         :param date: the string representation of the ClassTime
-        :returns: a corresponding ClassTime object
+        :returns: a corresponding ClassTime object if valid, None otherwise
         """
-        pass
+        match = re.fullmatch(REGEX_CLASSTIME, date.strip())
+
+        if not match:
+            return None
+
+        time = thisClass()
+        time.days = match.group(INDEX_DAYS)
+        time.setTimes(match.group(INDEX_START), match.group(INDEX_FINISH))
+
+        return time
 
     def setTimes(self, newStart, newFinish):
         """
@@ -32,6 +86,7 @@ class ClassTime:
         :param newStart: the date/time string for when the class starts
         :param newFinish: the date/time string for when the class finishes
         """
+        # Delegate the parsing and setting to setStart and setFinish.
         self.setStart(newStart)
         self.setFinish(newFinish)
 
@@ -41,8 +96,25 @@ class ClassTime:
 
         :param self: the ClassTime object
         :param newStart: the start time date/time string
+        :returns: True if the start time was set, False otherwise
         """
-        pass
+        # Make sure the start time is valid.
+        newStart = newStart.strip().lower()
+        match = re.match(REGEX_TIME, newStart)
+
+        if not match:
+            return False
+
+        # Get the hours and minutes as numbers.
+        hours = int(match.group(INDEX_HOURS))
+        minutes = int(match.group(INDEX_MINS)) % MAX_MINS
+        period = match.group(INDEX_PERIOD)
+
+        # Set the numeric start time.
+        self.startTime = ClassTime.toMinutes(hours, minutes, period)
+        self.start = newStart
+
+        return (self.startTime > -1)
 
     def setFinish(self, newFinish):
         """
@@ -51,7 +123,59 @@ class ClassTime:
         :param self: the ClassTime object
         :param newStart: the finish time date/time string
         """
-        pass
+        # Make sure the finish time is valid.
+        newFinish = newFinish.strip().lower()
+        match = re.match(REGEX_TIME, newFinish)
+
+        if not match:
+            return False
+
+        # Get the hours and minutes as numbers.
+        hours = int(match.group(INDEX_HOURS))
+        minutes = int(match.group(INDEX_MINS))
+        period = match.group(INDEX_PERIOD).lower()
+
+        # Set the numeric start time.
+        self.finishTime = ClassTime.toMinutes(hours, minutes, period)
+        self.finish = newFinish
+
+        return (self.finishTime > -1)
+
+    def isTimeBefore(self, other):
+        """
+        Returns whether or not the time for this ClassTime is before the time of
+        the given ClassTime object. This returns False if the other object is
+        not a ClassTime object.
+
+        :param self: the ClassTime object
+        :param other: the other ClassTime object to compare with
+        :returns: whether or not this ClassTime occurs before
+        """
+        if type(other) is ClassTime:
+            return (self.startTime < other.startTime and
+                    self.finishTime < other.startTime and
+                    self.startTime < other.finishTime and
+                    self.finishTime < other.finishTime)
+
+        return False
+
+    def isTimeAfter(self, other):
+        """
+        Returns whether or not the time for this ClassTime is after the time of
+        the given ClassTime object. This returns False if the other object is
+        not a ClassTime object.
+
+        :param self: the ClassTime object
+        :param other: the other ClassTime object to compare with
+        :returns: whether or not this ClassTime occurs after
+        """
+        if type(other) is ClassTime:
+            return (self.startTime > other.finishTime and
+                    self.finishTime > other.finishTime and
+                    self.startTime > other.startTime and
+                    self.finishTime > other.startTime)
+
+        return False
 
     def conflictsWith(self, other):
         """
@@ -62,7 +186,12 @@ class ClassTime:
         :param other: the other ClassTime object to compare with
         :returns: whether or not there is any time overlap (if any days match)
         """
-        return False
+        # Check day conflicts before time conflicts.
+        if not self.isOnDay(other.days):
+            return False
+
+        return (self.startTime <= other.finishTime and
+                other.startTime <= self.finishTime)
 
     def isOnDay(self, day):
         """
@@ -73,28 +202,10 @@ class ClassTime:
         :param day: a day of the week
         :returns: whether or not the ClassTime occurs on the given day
         """
-        return False
+        for day in re.findall(REGEX_DAY, day):
+            if self.days.find(day) > -1:
+                return True
 
-    def isTimeBefore(self, other):
-        """
-        Returns whether or not the time for this ClassTime is before the time of
-        the given ClassTime object.
-
-        :param self: the ClassTime object
-        :param other: the other ClassTime object to compare with
-        :returns: whether or not this ClassTime occurs before
-        """
-        return False
-
-    def isTimeAfter(self, other):
-        """
-        Returns whether or not the time for this ClassTime is after the time of
-        the given ClassTime object.
-
-        :param self: the ClassTime object
-        :param other: the other ClassTime object to compare with
-        :returns: whether or not this ClassTime occurs after
-        """
         return False
 
     def toString(self):
@@ -107,7 +218,7 @@ class ClassTime:
         :param self: the ClassTime object
         :returns: the string representation
         """
-        return ""
+        return self.days + " " + self.start + "-" + self.finish
 
     def __str__(self):
         return self.toString()
--- tritonschedule/schedule.py
+++ tritonschedule/schedule.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
-class Schedule:
+import re
+
+BASE_URL = "https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesFaculty" \
+           "Result.htm"
+TERM_REGEX = "([A-Z][0-9A-Z])(\d\d)"
+NEW_LINE = "%0D%0A"
+VALID_TERMS = set(["FA", "WI", "SU", "SP", "SA", "S3", "S2", "S1"])
+
+class Schedule(object):
     courses = []
-    term = "FA16"
+    term = "FA00"
+
+    def setTerm(self, newTerm):
+        """
+        Validates the given term to ensure it is in the proper format for the
+        Schedule of Classes and sets the schedule's term to it if it is valid.
+
+        :param self: the schedule object
+        :param newTerm: the desired term for the schedule
+        :returns: True if the term is valid, False otherwise
+        """
+        if not self.validateTerm(newTerm):
+            return False
+
+        self.term = newTerm
+
+        return True
 
     @staticmethod
-    def validateTerm(self, newTerm):
+    def validateTerm(newTerm):
         """
         Checks whether or not a given term code is in the format of a two letter
         valid season followed by a two digit year.
@@ -14,17 +38,28 @@ class Schedule:
         :param newTerm: the term code to check
         :returns: True if the given term is valid, False otherwise
         """
-        return False
+        newTerm = newTerm.upper()
+        match = re.match(TERM_REGEX, newTerm)
+
+        if not match:
+            return False
+
+        return (match.group(1) in VALID_TERMS)
 
     def getScheduleURL(self):
         """
         Creates a URL that searches for the specified courses at the given term
-        using the UCSD Schedule of Classes.
+        using the UCSD Schedule of Classes. This will return an empty string if
+        the list of desired courses is empty.
 
         :param self: the schedule object
         :returns: the URL as a string if successful, empty string otherwise
         """
-        return ""
+        if len(self.courses) == 0:
+            return ""
+
+        return (BASE_URL + "?selectedTerm=" + self.term + "&courses=" +
+                NEW_LINE.join(self.courses).replace(" ", "+"))
 
     def retrieve(self):
         """
